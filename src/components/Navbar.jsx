a13165f72a4310978533596ddc399578ae682f0c
Tonmoy <tonmoy@Tonmoy.Tonmoy>
2025-12-18 12:42:10 +0600

    Added theme and font customization and date-fns integration

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import logo from '../assets/logo.png'
+import { format } from 'date-fns';
 
 const Navbar = () => {
     return (
         <div className='flex flex-col justify-center items-center '>
             <img className='w-100' src={logo} alt="" />
-            <p>Journalism Without Fear or Favour</p>
+            <p className='text-accent'>Journalism Without Fear or Favour</p>
+            <p className='text-accent'>{format(new Date(), "EEEE, LLLL dd, yyyy")}</p>
         </div>
     );
 };
