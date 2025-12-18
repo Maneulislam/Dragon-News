9936e6c89d7971ffcf1ad22f692bd309a5a01fbd
Tonmoy <tonmoy@Tonmoy.Tonmoy>
2025-12-18 16:25:04 +0600

    Added navbar design

--- src/layouts/HomeLayout.jsx
+++ src/layouts/HomeLayout.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Header from '../components/Header';
 import Latest from '../components/Latest';
+import Navbar from '../components/Navbar';
 
 const HomeLayout = () => {
     return (
@@ -8,6 +9,7 @@ const HomeLayout = () => {
             <header>
                 <Header></Header>
                 <Latest></Latest>
+                <Navbar></Navbar>
             </header>
 
             <main>
